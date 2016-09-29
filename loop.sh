a713aa80a883638861b5e1b237ffeeda75c74cab
pasmax2 <pasmax2@gmail.com>
2016-09-29 12:45:40 -0400

    Add files via upload

---
+++ loop.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+   BINDIR="$(dirname "\$(readlink -fn "\$0")")"
+   cd "$BINDIR"
+   while true
+   do
+	java -Xincgc -Xmx6G -jar $1
+      echo "Si vous voulez arreter completement le processus de serveur maintenant, appuyez sur Ctrl + C avant le temps est ecoule! "
+      echo "Redemarrage dans:"
+      for i in 3 2 1
+      do
+            echo "$i..."
+            sleep 1
+      done
+      echo "Redemarrage maintenant!"
+   done
---
+++ start.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+Version="1.0"
+ScreenName="mine.off"
+Jar="craftbukkit-1.8-R0.1.jar"
+
+condition()
+{
+	case $1 in
+	stop)
+		if ! screen -list | grep -q $ScreenName; then
+		  echo -e "\033[31mUn screen n'es pas actif vous devez le démaré avant!"
+		  exit 0
+		else
+			screen -r -S $ScreenName -X quit
+			exit 0
+		fi
+	;;
+	esac
+		
+	if screen -list | grep -q $ScreenName; then
+	  echo -e "\033[31mUn screen est déjà actif vous devez le quitter avant!"
+	  exit 0
+	fi
+}
+
+
+condition $1
+
+
+	screen -dmSL $ScreenName ./loop.sh $Jar
