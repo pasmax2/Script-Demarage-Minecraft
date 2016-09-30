591b731c627a19b155f29ca97e4e9ef4409fbcc6
pasmax2 <pasmax2@gmail.com>
2016-09-30 02:14:51 -0400

    Édition script

--- start.sh
+++ start.sh
@@ -1,8 +1,18 @@
 #!/bin/sh
 Version="1.1"
-ScreenName="mine.off"
-Jar="craftbukkit-1.8-R0.1.jar"
-#source="system.cfg" #Faire en sorte que le fichier sois automatiquement installer
+# ScreenName="mine.off"
+# Jar="craftbukkit-1.8-R0.1.jar"
+if [ -e $PWD/systeme.conf ]
+{
+    source="$PWD/systeme.conf" #Faire en sorte que le fichier sois automatiquement installer
+}
+else
+{
+    read -p "Entré le ScreenName : " ScreenName
+    read -p "Entré le fichier Jar : " Jar
+    echo "ScreenName='$ScreenName'" > $PWD/systeme.conf
+    echo "Jar='craftbukkit-1.8-R0.1.jar'" >> $PWD/systeme.conf
+}
 condition()
 {
 	case $1 in
@@ -21,6 +31,25 @@ condition()
 	  echo -e "\033[31mUn screen est déjà actif vous devez le quitter avant!"
 	  exit 0
 	fi
+}
+loop()
+{
+echo "Démarage du script Version:$2"
+
+   BINDIR="$(dirname "\$(readlink -fn "\$0")")"
+   cd "$BINDIR"
+   while true
+   do
+	java -Xincgc -Xmx6G -jar $1
+      echo "Si vous voulez arreter completement le processus de serveur maintenant, appuyez sur Ctrl + C avant le temps est ecoule! "
+      echo "Redemarrage dans:"
+      for i in 3 2 1
+      do
+            echo "$i..."
+            sleep 1
+      done
+      echo "Redemarrage maintenant!"
+   done
 }
 
 
