c6a996e3cda1c33c541ab436a454a299a12a9e3f
pasmax2 <pasmax2@gmail.com>
2016-09-30 04:29:28 -0400

    Ajout de la function démarage unique avec protection sur le system et corigé un bug à l'ouverture du serveur et uptimise le systeme de démarage

--- start.sh
+++ start.sh
@@ -4,42 +4,50 @@ Version="1.1"
 # Jar="craftbukkit-1.8-R0.1.jar"
 
 if [ -e $PWD/systeme.conf ]; then
-    source=$PWD/systeme.conf #Faire en sorte que le fichier sois automatiquement installer
+    echo "Chargement du fichier de configuration"
+    source $PWD/systeme.conf #Faire en sorte que le fichier sois automatiquement installer
 else
     read -p "Entré le ScreenName : " ScreenName
     read -p "Entré le fichier Jar : " Jar
+    read -p "Combien de ram attribué : " Ram
     echo "ScreenName=\"$ScreenName\"" > $PWD/systeme.conf
     echo "Jar=\"$Jar\"" >> $PWD/systeme.conf
+    echo "Ram=\"$Ram\"" >> $PWD/systeme.conf
+    echo "Fichier de config crée"
+    exit 0
 fi
 
-condition()
+function condition()
 {
 	case $1 in
 	stop)
-		if ! screen -list | grep -q $ScreenName; then
+		if ! screen -list | grep -q $2; then
 		  echo -e "\033[31mUn screen n'es pas actif vous devez le démaré avant!"
 		  exit 0
 		else
-			screen -r -S $ScreenName -X quit
+			screen -r -S $2 -X quit
 			exit 0
 		fi
 	;;
+    start)
+        loop $3 $4
+    ;;
 	esac
 		
-	if screen -list | grep -q $ScreenName; then
+	if screen -list | grep -q $2; then
 	  echo -e "\033[31mUn screen est déjà actif vous devez le quitter avant!"
 	  exit 0
 	fi
 }
-loop()
+function loop()
 {
-echo "Démarage du script Version:$2"
-
+echo "Démarage de la protection anti crash"
+echo "$(date)"
    BINDIR="$(dirname "\$(readlink -fn "\$0")")"
    cd "$BINDIR"
    while true
    do
-	java -Xincgc -Xmx6G -jar $1
+      java -Xincgc -Xmx"$2"G -jar $1
       echo "Si vous voulez arreter completement le processus de serveur maintenant, appuyez sur Ctrl + C avant le temps est ecoule! "
       echo "Redemarrage dans:"
       for i in 3 2 1
@@ -50,9 +58,11 @@ echo "Démarage du script Version:$2"
       echo "Redemarrage maintenant!"
    done
 }
+if [ -z $1 ]; then
+var="false"
+else
+var=$1
+fi
+condition $var $ScreenName $Jar $Ram
 
-
-condition $1
-
-
-	screen -dmSL $ScreenName loop $Jar $Version
+	screen -dmSL $ScreenName $0 'start' $Jar $Ram
