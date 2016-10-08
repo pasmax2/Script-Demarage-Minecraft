a9def5320a734d475321728e22474b66a7dba1b4
DJBlack <pasmax2@gmail.com>
2016-10-08 04:51:20 -0400

    Update

--- update.sh
+++ update.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
+StartURL=https://raw.githubusercontent.com/pasmax2/Script-Demarage-Minecraft/Dev/start.sh
 echo "[X-C4 - UPGRADE] Mise à jour du système en cours"
+rm $PWD/start.sh
+echo "[X-C4 - UPGRADE] Téléchargement du nouveaux système"
+wget -q $StartURL -N
+echo "[X-C4 - UPGRADE] Démarage du système en cours"
+source $PWD/start.sh
