c43206b5d7e9cebb044f29d9e8c1212cb155e9cc
DJBlack Phoénix <pasmax2@users.noreply.github.com>
2021-02-06 11:19:31 -0500

    optimise les tabulation
    ajouter les tabulation au endroit pour que le code sois mieux lisible.

--- start.sh
+++ start.sh
@@ -20,18 +20,18 @@ fi
 function condition()
 {
 	case $1 in
-	stop)
-		if ! screen -list | grep -q $2; then
-		  echo -e "\033[31mUn screen n'es pas actif vous devez le démaré avant!"
-		  exit 0
-		else
-			screen -r -S $2 -X quit
-			exit 0
-		fi
-	;;
-    start)
-        loop $3 $4
-    ;;
+		stop)
+			if ! screen -list | grep -q $2; then
+			  echo -e "\033[31mUn screen n'es pas actif vous devez le démaré avant!"
+			  exit 0
+			else
+				screen -r -S $2 -X quit
+				exit 0
+			fi
+		;;
+		start)
+			loop $3 $4
+		;;
 	esac
 		
 	if screen -list | grep -q $2; then
@@ -41,28 +41,29 @@ function condition()
 }
 function loop()
 {
-echo "Démarage de la protection anti crash"
-echo "$(date)"
-   BINDIR="$(dirname "\$(readlink -fn "\$0")")"
-   cd "$BINDIR"
-   while true
-   do
-      java -Xincgc -Xmx"$2"G -jar $1
-      echo "Si vous voulez arreter completement le processus de serveur maintenant, appuyez sur Ctrl + C avant le temps est ecoule! "
-      echo "Redemarrage dans:"
-      for i in 3 2 1
-      do
-            echo "$i..."
-            sleep 1
-      done
-      echo "Redemarrage maintenant!"
-   done
+	echo "Démarage de la protection anti crash"
+	echo "$(date)"
+	BINDIR="$(dirname "\$(readlink -fn "\$0")")"
+	cd "$BINDIR"
+	while true
+		do
+		java -Xincgc -Xmx"$2"G -jar $1
+		echo "Si vous voulez arreter completement le processus de serveur maintenant, appuyez sur Ctrl + C avant le temps est ecoule! "
+		echo "Redemarrage dans:"
+		for i in 3 2 1
+		do
+		    echo "$i..."
+		    sleep 1
+		done
+		echo "Redemarrage maintenant!"
+	done
 }
 if [ -z $1 ]; then
-var="false"
+	var="false"
 else
-var=$1
+	var=$1
 fi
+
 condition $var $ScreenName $Jar $Ram
 
 	screen -dmSL $ScreenName $0 'start' $Jar $Ram
